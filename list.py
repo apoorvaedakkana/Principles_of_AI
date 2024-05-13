421544ded34aec023574b07ff89ef23ef7a8e6d4
apoorvaedakkana <156586361+apoorvaedakkana@users.noreply.github.com>
2024-05-13 21:16:50 +0530

    list.py

--- list.py
+++ list.py
@@ -3,17 +3,17 @@ fruits = ["apple", "banana"]
 print("Initial list:", fruits)
 
 
-fruits.append("orange")  # Append "orange" to the end of the list
+fruits.append("orange")  
 print("After appending 'orange':", fruits)
 
 
-fruits.insert(1, "grape")  # Insert "grape" at index 1
+fruits.insert(1, "grape") 
 print("After inserting 'grape' at index 1:", fruits)
 
 
-fruits.remove("apple")  # Remove the first occurrence of "apple" from the list
+fruits.remove("apple") 
 print("After removing 'apple':", fruits)
 
 
-del fruits[0]  # Delete the element at index 0
+del fruits[0] 
 print("After deleting the element at index 0:", fruits)
