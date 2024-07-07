a7b96f0fe228e4527542ace9410bf36c3022255e
apoorvaedakkana <156586361+apoorvaedakkana@users.noreply.github.com>
2024-07-07 12:19:57 +0530

    removelast.py

---
+++ removelast.py
@@ -0,0 +1,11 @@
+my_list = [1, 2, 3, 4, 5]
+last_element = my_list.pop()  # Removes and returns the last element
+print("List after removing the last element:", my_list)
+print("Removed element:", last_element)
+
+#deletes all element
+
+my_list = [1, 2, 3, 4, 5]
+my_list.clear()  # Removes all elements from the list
+
+print("List after deleting all elements:", my_list)
