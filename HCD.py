de42b67c2992d490648054a03b37faf04c3d9f6e
apoorvaedakkana <156586361+apoorvaedakkana@users.noreply.github.com>
2024-07-07 11:54:52 +0530

    Create HCF.py

---
+++ HCD.py
@@ -0,0 +1,10 @@
+import math
+
+def hcf(x, y):
+    return math.gcd(x, y)
+
+# Example usage
+num1 = 24
+num2 = 36
+result_hcf = hcf(num1, num2)
+print(f"The HCF of {num1} and {num2} is: {result_hcf}")
